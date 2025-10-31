c7424cafc5724d6b3d39d4f084b31a3ff20463fe
Artur <arturvovk780@gmail.com>
2025-10-31 13:28:00 +0200

    fix argument in split_integer

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -2,7 +2,7 @@ from app.split_integer import split_integer
 
 
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    result = split_integer(value=17, number_of_parts=4)
+    result = split_integer(17, 4)
     assert sum(result) == 17
     assert len(result) == 4
     assert max(result) - min(result) <= 1
@@ -10,20 +10,20 @@ def test_sum_of_the_parts_should_be_equal_to_value() -> None:
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    result = split_integer(value=5, number_of_parts=5)
+    result = split_integer(5, 5)
     assert len(result) == 5
     assert max(result) == min(result)
     assert sum(result) == 5
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    result = split_integer(value=8, number_of_parts=1)
+    result = split_integer(8, 1)
     assert len(result) == 1
     assert result[0] == 8
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    result = split_integer(value=17, number_of_parts=4)
+    result = split_integer(17, 4)
     assert len(result) == 4
     assert sum(result) == 17
     assert result == sorted(result)
@@ -31,7 +31,7 @@ def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    result = split_integer(value=3, number_of_parts=6)
+    result = split_integer(3, 6)
     assert len(result) == 6
     assert sum(result) == 3
     assert min(result) == 0
